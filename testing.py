7c37f5128c449eed2a8fbc5517436e318d981edc
Noah <westnoahb@gmail.com>
2024-09-17 23:16:44 -0400

    tweak testing format

--- testing.py
+++ testing.py
@@ -7,48 +7,54 @@ import sys
 import numpy as np
 from tkinter.filedialog import askopenfile
 from matplotlib import pyplot as plt
+import GcodeToPath
 
 
-# Select and load the CSV file
-file_path = askopenfilename(filetypes=[("CSV files", "*.csv")],
-                            initialdir="C:/Users/westn/OneDrive - Widener University/Research/Nagel Lab/Dual Stage 3D printer/pathCSVs")
+def main():
+    plotFromFile()
 
-# Load the CSV file into a NumPy array
-data = np.genfromtxt(file_path, delimiter=',', skip_header=1)
 
-# Check the shape of the data to understand its structure
-print("Data shape:", data.shape)
+def plotFromFile():
+    # Select and load the CSV file
+    file_path = askopenfilename(filetypes=[("CSV files", "*.csv")],
+                                initialdir="C:/Users/westn/OneDrive - Widener University/Research/Nagel Lab/Dual Stage 3D printer/pathCSVs")
 
-# Extract columns
-time = data[:, 0]
-xREF = data[:, 1]
-xLRA = data[:, 5]
-xSRA = data[:, 7]
-yREF = data[:, 2]
-yLRA = data[:, 6]
-ySRA = data[:, 8]
+    # Load the CSV file into a NumPy array
+    data = np.genfromtxt(file_path, delimiter=',', skip_header=1)
 
-print(f"xSRA max: {np.max(np.abs(xSRA))}")
-print(f"ySRA max: {np.max(np.abs(ySRA))}")
+    # Check the shape of the data to understand its structure
+    print("Data shape:", data.shape)
 
-# Plotting columns
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
-ax1.plot(time, xREF, linestyle='-', color='r')
-ax1.plot(time, xLRA, linestyle='-', color='b')
-ax1.plot(time, xSRA, linestyle='-', color='g')
-ax1.set_title('Filtered ')
-ax1.set_xlabel('time(s)')
-ax1.set_ylabel('x(mm)')
+    # Extract columns
+    time = data[:, 0]
+    xREF = data[:, 1]
+    xLRA = data[:, 5]
+    xSRA = data[:, 7]
+    yREF = data[:, 2]
+    yLRA = data[:, 6]
+    ySRA = data[:, 8]
 
-ax2.plot(time, yREF, linestyle='-', color='r')
-ax2.plot(time, yLRA, linestyle='-', color='b')
-ax2.plot(time, ySRA, linestyle='-', color='g')
-ax2.set_title('Filtered ')
-ax2.set_xlabel('time(s)')
-ax2.set_ylabel('x(mm)')
+    print(f"xSRA max: {np.max(np.abs(xSRA))}")
+    print(f"ySRA max: {np.max(np.abs(ySRA))}")
 
-plt.grid(True)
-plt.show()
+    # Plotting columns
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
+    ax1.plot(time, xREF, linestyle='-', color='r')
+    ax1.plot(time, xLRA, linestyle='-', color='b')
+    ax1.plot(time, xSRA, linestyle='-', color='g')
+    ax1.set_title('Filtered ')
+    ax1.set_xlabel('time(s)')
+    ax1.set_ylabel('x(mm)')
+
+    # ax2.plot(time, yREF, linestyle='-', color='r')
+    # ax2.plot(time, yLRA, linestyle='-', color='b')
+    # ax2.plot(time, ySRA, linestyle='-', color='g')
+    # ax2.set_title('Filtered ')
+    # ax2.set_xlabel('time(s)')
+    # ax2.set_ylabel('x(mm)')
+
+    plt.grid(True)
+    plt.show()
 
 
 # Apply the convolution with 'full' mode (zero-padding at the edges)
@@ -74,3 +80,6 @@ plt.show()
 # print(len(pos_arr))
 # pos_arr = p.parse_line("G4 P550")
 # print(pos_arr)
+
+if __name__ == "__main__":
+    exit(main())
